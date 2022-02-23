365f57493c1f4fcce83e59f0e7d2ce78fe1a3a2f
colemclark1 <clark.cole@husky.neu.edu>
2022-02-23 11:29:58 -0500

    updating project grid

--- src/components/Project/ProjectNew.js
+++ src/components/Project/ProjectNew.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 import { Row } from "../Utils/Utils"
 import style from "./ProjectNew.module.css";
+import { Collapse } from 'react-bootstrap'
 
 const ProjectNew = (props) => {
-  const [displayDetails, setDisplayDetails] = React.useState(false);
   return (
-    <div className={style.card}>
+    <>
+    <div className={`${style.card} ${props.display ? style.active : ''}`} onClick={props.setDisplayIndex} >
         <img src={props.project.image} className={style.picture}></img>
         <div className={style.body}>
         <div className={style.title}>{props.project.title}</div>
         <div className={style.type}>{props.project.type}</div>
         </div>
     </div>
+    
+    {
+    <Collapse in={props.display}>
+    <div className={`${style.fullWidthCard} ${props.display ? style.active : ''}`}  >
+      <div>{props.project.textShort}</div>
+
+
+    </div>
+    
+    </Collapse>}
+
+    </>
   );
 };
 
--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -6,13 +6,23 @@ import { Grid } from "../Utils/Utils";
 import style from "./Projects.module.css"
 
 const Projects = () => {
+  const [displayIndex, setDisplayIndex] = React.useState(null);
+
+  const setDisplay = (index) => {
+    if (index === displayIndex) {
+      setDisplayIndex(null)
+    } else {
+      setDisplayIndex(index)
+    }
+  }
+
   return (
     <div className="container-fluid">
       <div className="container col-11 text-center">
         <div className="row header page-headers">Projects</div>
         <div className={style.projects}>
           {projects.map((project, index) => 
-            <ProjectNew key={index} index={index} project={project} />
+            <ProjectNew key={index} display={index === displayIndex} setDisplayIndex={() => setDisplay(index)} project={project} />
           )}
         </div>
       </div>
